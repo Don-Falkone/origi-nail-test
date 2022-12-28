3cc3b429d7e29baa7926b2788d31d36270c58d85
oleh <oleh.a.falko@gmail.com>
2022-12-28 19:23:13 +0100

    gallery added

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const sendMsgAPI = "https://api.callmebot.com/text.php?user=@oolezha&text=";
+const sliderImages = [
+  "./assets/images/alena_khromova_1661228674447711.jpg",
+  "./assets/images/IMG_4737.jpg",
+  "./assets/images/IMG_5154-2.JPG",
+];
+
+const active = "white";
+const inactive = "#f5f5f5a5";
 
 const form = document.querySelector("form");
+const leftBtn = document.querySelector("#left");
+const rightBtn = document.querySelector("#right");
+
+let currentID = 0;
 
 form.addEventListener("submit", (e) => sendMsg(e));
+rightBtn.addEventListener("click", () => changePhoto(1));
+leftBtn.addEventListener("click", () => changePhoto(-1));
 
 function openPrice(name) {
   const popUp = document.querySelector(".pop-up");
@@ -34,4 +48,28 @@ function sendMsg(event) {
     console.log("Error: " + e);
   });
   closeBook();
+}
+
+function changePhoto(step) {
+  const slider = document.querySelector("#slider-image");
+  currentID = (3 + (currentID + step)) % 3;
+  console.log(currentID);
+  switch (currentID) {
+    case 0:
+      document.documentElement.style.setProperty("--before-color", active);
+      document.documentElement.style.setProperty("--main-color", inactive);
+      document.documentElement.style.setProperty("--after-color", inactive);
+      break;
+    case 1:
+      document.documentElement.style.setProperty("--before-color", inactive);
+      document.documentElement.style.setProperty("--main-color", active);
+      document.documentElement.style.setProperty("--after-color", inactive);
+      break;
+    case 2:
+      document.documentElement.style.setProperty("--before-color", inactive);
+      document.documentElement.style.setProperty("--main-color", inactive);
+      document.documentElement.style.setProperty("--after-color", active);
+      break;
+  }
+  slider.src = sliderImages[currentID];
 }
